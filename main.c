2822eca86d90dfaeea6ab7a6f69c525ff551767b
Matheus Citeli <matheus.citeli2001@gmail.com>
2025-02-21 19:08:18 -0300

    :sparkles: FEAT: implementação de argumentos

--- main.c
+++ main.c
@@ -1,35 +1,47 @@
 #include "src/Alias_list.h"
 #include <string.h>
 
-/*
-printf("Uso: %s <comando> [alias] [comando]\n", argv[0]);
-printf("Comandos:\n");
-printf("  add <alias> <comando> - Adiciona um novo alias\n");
-printf("  list - Lista todos os aliases\n");
-printf("  exec <alias> - Executa o comando associado ao alias\n");
-printf("  rm <alias> - Remove um alias\n");
-*/
+// Exibe o uso do programa
+void exibir_uso(const char *prog) {
+    printf("Uso: %s <comando> [alias] [comando]\n", prog);
+    printf("Comandos:\n");
+    printf("  add <alias> \"<comando>\"       - Adiciona um novo alias\n");
+    printf("  list                          - Lista todos os aliases\n");
+    printf("  exec <alias>                  - Executa o comando associado ao alias\n");
+    printf("  rm <alias>                    - Remove um alias\n");
+}
 
 int main(int argc, char *argv[]){
 
     Alias_list *list = load_alias_list();
 
-    // Alias_list *list = NULL;
-    // char *comando = "cd ~/Programação/JavaScript/Residência/Reforma && code . && echo \"Vai trabalhar na residencia campeão, você é foda!\"";
+    if(argc < 2){
+        exibir_uso(argv[0]);
+        return 1;
+    }
 
-    // Alias api_back;
-    // api_back = create_alias("api", comando);
+    // ./idx <comando> [alias] [comando]
+    if (strncmp(argv[1], "add", 3) == 0) {
+        Alias alias = create_alias(argv[2], argv[3]);
+        print_alias(alias);
+        printf("argc: %d\n", argc);
+        list = add_alias(list, alias);
 
-    // list = add_alias(list, api_back);
+    } else if (strncmp(argv[1], "list", 4) == 0) {
+        list_alias(list);
 
-    // char *comando_motivacional = "echo \"Vai trabalhar na residencia campeão, você é foda!\"";
+    } else if (strncmp(argv[1], "exec", 4) == 0 && argc == 3) {
+        exec_alias(argv[2], list);
 
-    // Alias motivacional;
-    // motivacional = create_alias("motivacional", comando_motivacional);
+    } else if (strncmp(argv[1], "rm", 2) == 0 && argc == 3) {
+        remove_alias(list, argv[2]);
 
-    // list = add_alias(list, motivacional);
+    } else {
+        exibir_uso(argv[0]);
+        return 1;
+    }
 
-    list_alias(list);
-    exec_alias("motivacional", list);
+    printf("Salvando lista de aliases\n");
     save_alias_list(list);
+    return 0;
 }
